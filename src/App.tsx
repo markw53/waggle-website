a5dabfe4e9c75f9db1857d0812b5129918a345a1
‘markw53’ <m.workman@outlook.com>
2025-11-25 13:51:31 +0000

    aliases cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { ROUTES } from '@/config/routes';
 import 'leaflet/dist/leaflet.css';
-import CookieConsentBanner from './components/CookieConsentBanner';
+import CookieConsentBanner from '@components/CookieConsentBanner';
 
 // Layout Components
 import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
-import EmailVerificationBanner from '@/components/EmailVerificationBanner';
-import ResponsiveIndicator from '@/components/ResponsiveIndicator';
+import EmailVerificationBanner from '@components/EmailVerificationBanner';
+import ResponsiveIndicator from '@components/ResponsiveIndicator';
 
 // Auth Components
 import LoginForm from '@components/LoginForm';
 import RegisterForm from '@components/RegisterForm';
 import ResetPasswordForm from '@components/ResetPasswordForm';
-import VerifyEmail from './pages/VerifyEmail';
+import VerifyEmail from '@pages/VerifyEmail';
 
 // Route Guards
 import RequireAuth from '@/routes/RequireAuth';
@@ -23,39 +23,39 @@ import RedirectIfAuth from '@/routes/RedirectIfAuth';
 
 // Main Pages
 import Dashboard from '@components/Dashboard';
-import Profile from './pages/Profile';
-import UserProfilePage from '@/pages/UserProfilePage';
-import GettingStarted from '@/pages/GettingStarted';
-import NotFound from './pages/NotFound';
-import BreedingCalendarPage from '@/pages/BreedingCalendarPage';
+import Profile from '@pages/Profile';
+import UserProfilePage from '@pages/UserProfilePage';
+import GettingStarted from '@pages/GettingStarted';
+import NotFound from '@pages/NotFound';
+import BreedingCalendarPage from '@pages/BreedingCalendarPage';
 
 // Dog Pages
-import DogSearch from '@/pages/DogSearch';
-import DogProfile from '@/pages/DogProfile';
+import DogSearch from '@pages/DogSearch';
+import DogProfile from '@pages/DogProfile';
 import AddDog from '@pages/AddDog';
-import MyDogs from './pages/MyDogs';
-import DogsMapPage from './pages/DogsMapPage';
-import EditDog from '@/pages/EditDog';
+import MyDogs from '@pages/MyDogs';
+import DogsMapPage from '@pages/DogsMapPage';
+import EditDog from '@pages/EditDog';
 
 // Breed Pages
-import BreedDirectory from './pages/BreedDirectory';
-import BreedProfile from './pages/BreedProfile';
+import BreedDirectory from '@pages/BreedDirectory';
+import BreedProfile from '@pages/BreedProfile';
 
 // Match Pages
 import AddMatch from '@pages/AddMatch';
 import MatchesList from '@pages/MatchesList';
 
 // Messaging
-import Messages from '@/pages/Messages';
-import ConversationPage from '@/pages/Conversation';
+import Messages from '@pages/Messages';
+import ConversationPage from '@pages/Conversation';
 
 // Analytics & Admin
-import Analytics from '@/pages/Analytics';
-import AdminDashboard from '@/pages/AdminDashboard';
+import Analytics from '@pages/Analytics';
+import AdminDashboard from '@pages/AdminDashboard';
 
 // Legal
-import PrivacyPolicy from '@/pages/PrivacyPolicy';
-import TermsOfService from '@/pages/TermsOfService';
+import PrivacyPolicy from '@pages/PrivacyPolicy';
+import TermsOfService from '@pages/TermsOfService';
 
 const App: React.FC = () => (
   <div className="min-h-screen w-full bg-cover bg-center bg-fixed bg-[url('/waggle-background.png')] dark:bg-zinc-900 flex flex-col">
